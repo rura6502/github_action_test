6ff457f609956227aa8ebe55dc201454990eb757
rura6502 <rura6502@hotmail.com>
2022-02-15 17:30:59 +0900

    init project

---
+++ src/main/java/io/github/rura6502/github_action_test/GithubActionTestApplication.java
@@ -0,0 +1,15 @@
+package io.github.rura6502.github_action_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(GithubActionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rura6502/github_action_test/GithubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rura6502.github_action_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
