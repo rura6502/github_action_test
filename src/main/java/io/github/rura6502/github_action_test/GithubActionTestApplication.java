31eb13e746c992907e6e0066fc5b8f52ebb3b25e
rura6502 <rura6502@hotmail.com>
2022-02-17 17:34:38 +0900

    badafgsdfsadfwqf

--- src/main/java/io/github/rura6502/github_action_test/GithubActionTestApplication.java
+++ src/main/java/io/github/rura6502/github_action_test/GithubActionTestApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionTestApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(GithubActionTestApplication.class, args);
     }
 
