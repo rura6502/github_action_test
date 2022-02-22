96385409f528c44b5268a499db06353aac1e7b0f
rura6502 <rura6502@hotmail.com>
2022-02-22 16:56:46 +0900

    test

--- src/main/java/io/github/rura6502/github_action_test/GithubActionTestApplication.java
+++ src/main/java/io/github/rura6502/github_action_test/GithubActionTestApplication.java
@@ -1,11 +1,23 @@
 package io.github.rura6502.github_action_test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class GithubActionTestApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionTestApplication.class, args);
+
+
+	public static boolean test(int v) {
+		if (v % 2 == 0) {
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+			System.out.println(5);
+			System.out.println(6);
+			System.out.println(7);
+			return true;
+		} else {
+			System.out.println(8);
+			System.out.println(9);
+			System.out.println(10);
+			return false;
+		}
 	}
 }
